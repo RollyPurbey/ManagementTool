b765dcba3e133f880ef7e5b9db9d294c9ee30d7a
Rolly Purbey <purbey.rolly@gmail.com>
2020-12-15 00:34:43 +0530

    First Commit

---
+++ managementtool/src/test/java/com/simplelearning/managementtool/ManagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplelearning.managementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
